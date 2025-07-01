c21424d7c262af910b05f3c5e9e6413a086e8c6e
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-07-01 14:49:05 -0300

    Add files via upload

---
+++ Informacao/informacao.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome = "Matheus"
+	var idade = 17
+	var altura = 1.84
+	var peso = 65.2
+	var estudante = true
+
+	fmt.Println("Seu nome é:", nome)
+	fmt.Println("Sua idade é:", idade)
+	fmt.Println("Sua altura é:", altura)
+	fmt.Println("Seu peso é:", peso)
+	fmt.Println("Estuda:", estudante)
+}
