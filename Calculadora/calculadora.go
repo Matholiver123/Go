594d8ce0caadc1d21a2f191cbb02fbc7e249b86d
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-07-03 12:11:10 -0300

    Add files via upload

---
+++ Calculadora/calculadora.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//declaração de variáveis
+	num1 := 100
+	num2 := 20
+	soma := num1 + num2
+	subtracao := num1 - num2
+	multiplicacao := num1 * num2
+	divisao := num1 / num2
+
+	fmt.Println("A soma dos dois números será: ", soma)
+	fmt.Println("A subtração dos dois números será: ", subtracao)
+	fmt.Println("A multiplicacao dos dois números será: ", multiplicacao)
+	fmt.Println("A divisão dos dois números será: ", divisao)
+
+}
